08ee594682d064bb47a882906c207b9416592622
bathy <bathyg@gmail.com>
2018-05-02 15:16:27 -0700

    test commit

--- regex.py
+++ regex.py
@@ -4,5 +4,5 @@ reg_ex='([KR](?=[^P]))|((?<=W)K(?=P))|((?<=M)R(?=P))'
 
 cut_sequence=re.finditer(reg_ex,'MASFLAVVGAPGGVNSCVFDRFHYSVKMGDRSVYNENTSVVVSVQRAFVHPKFSTVTTIRNDLALLQLQHPVNFTSNIQPICIPQENFQVEGRFWGPLGL')
 cleavagePosList = set(itertools.chain(map(lambda x: x.end(), cut_sequence)))
-
+#if works
 print cleavagePosList
